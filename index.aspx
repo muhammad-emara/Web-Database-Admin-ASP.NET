efd8a04e8592c0854ad89ce9659725482f7846e8
Muhammad Emara <muhammad.emara@gmail.com>
2020-09-08 11:25:46 +0400

    Index file

---
+++ index.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="index.aspx.cs" Inherits="DbMgmAdmin.index" validateRequest="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>.DataBase Management Admin:. - Welcome </title>
+
+    <%
+if (Session["Admin"] == "true")
+{
+Response.Redirect("dbmain.aspx");
+}
+else{
+ }
+%>
+</head>
+<body>
+    <center>
+			<script language="javascript">
+	function validate_length1(oSrc, args)
+	{
+		args.Isvalid=true;
+		if(args.Value)
+		{
+			var length = args.Value.length;
+			if( length<4)
+			{
+				args.IsValid=false;
+				
+			}
+		}
+	}</script>
+			<TABLE cellSpacing="0" cellPadding="0" width="798" border="0" bgColor="white">
+				<TR>
+					<TD width="605" colSpan="2">
+						<!--include file="includes/top.htm"--></TD>
+				</TR>
+				<TR>
+					<TD vAlign="top" width="405">
+						<TABLE border="1" id="Table1" height="300" cellSpacing="0" cellPadding="0" width="100%"
+							bordercolorlight="#ffffff" bordercolordark="#999999" bgColor="white" valign="top">
+							<tr vAlign="top" height="47" width="405">
+								<td><IMG alt="" src="images/Admin.gif"></td>
+							</tr>
+							<tr>
+								<td vAlign="top" align="center">
+									<form runat="server">
+										<TABLE id="Table1" height="112" cellSpacing="1" cellPadding="1" border="0">
+											<TR>
+												<TD colSpan="3">
+													&nbsp;</TD>
+											</TR>
+											<TR>
+												<TD colSpan="3">
+													<asp:Label id="lblError" runat="server" Font-Names="Arial" Font-Size="Smaller" ForeColor="Red"></asp:Label></TD>
+											</TR>
+											<TR>
+												<TD width="71"><asp:label id="EmailID" runat="server" Font-Size="10pt" Font-Names="Arial" Width="88px">Username</asp:label></TD>
+												<TD width="151"><asp:textbox id="txtemail" runat="server" Width="152px"></asp:textbox></TD>
+												<TD></TD>
+											</TR>
+											<TR>
+												<TD width="71"><asp:label id="Password" runat="server" Font-Size="10pt" Font-Names="Arial">Password</asp:label></TD>
+												<TD width="151"><asp:textbox id="txtpwd" runat="server" Width="152px" TextMode="Password"></asp:textbox></TD>
+												<TD></TD>
+											</TR>
+											<TR>
+												<TD width="71"></TD>
+												<TD width="151"><asp:button id="Log" runat="server" Text="Login"></asp:button>&nbsp;&nbsp;
+													<asp:button id="btnLogin" runat="server" Text="Reset"></asp:button></TD>
+												<TD></TD>
+											</TR>
+											<TR>
+												<td></td>
+												<TD colSpan="2"><FONT face="Verdana" size="1"><EM> <a href="ForgotPwd.aspx">
+																<span style="TEXT-DECORATION: none">
+																	<FONT color="#ff3300"></FONT></a></SPAN></EM></FONT></TD>
+											</TR>
+											
+											<TR>
+												
+												<TD colSpan="3"><FONT face="Arial" size="2">&nbsp</TD>
+											</TR>
+											
+										</TABLE>
+										
+									</form>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2"><!--#include file="includes/bot.htm"--></td>
+							</tr>
+						</TABLE>
+					</TD>
+				</TR>
+			</TABLE>
+		</center>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Configuration;
+using System.Text.RegularExpressions;
+/********************************************************************
+	
+    Author :         (Muhammad Emara - muhammad.emara@hotmail.com)
+	created:	08/09/2020
+	file base:	Index.aspx
+	purpose:	Login/ default page
+*********************************************************************/
+
+namespace DbMgmAdmin
+{
+    /// <summary>
+    /// Summary description for Template.
+    /// </summary>
+    public class Index : System.Web.UI.Page
+    {
+        protected System.Web.UI.WebControls.Label EmailID;
+        protected System.Web.UI.WebControls.Label Password;
+        protected System.Web.UI.WebControls.Button Log;
+        protected System.Web.UI.WebControls.TextBox txtemail;
+        protected System.Web.UI.WebControls.TextBox txtpwd;
+        protected System.Web.UI.WebControls.Label lblError;
+        protected System.Web.UI.WebControls.Button btnLogin;
+
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+            }
+            // Put user code to initialize the page here
+        }
+
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Log.Click += new System.EventHandler(this.Log_Click);
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.Load += new System.EventHandler(this.Page_Load);
+
+        }
+        #endregion
+
+        private void Log_Click(object sender, System.EventArgs e)
+        {
+
+            string uname;
+            uname = txtemail.Text.ToString();
+
+            string pass;
+            pass = txtpwd.Text.ToString();
+
+            string s = "none";
+            bool noErrorFlag = true;
+
+
+            noErrorFlag = noErrorFlag && CheckSQLInjection(uname);
+            //Response.Write(s + "<hr>1" + noErrorFlag + "<br>"); 
+
+            noErrorFlag = noErrorFlag && CheckSQLInjection(pass);
+            //Response.Write(s + "<hr>2" + noErrorFlag + "<br>"); 
+
+
+            if (noErrorFlag)
+            {
+                s = "SELECT * FROM " + ConfigurationSettings.AppSettings["AdminTable"] + " where Username ='" + uname + "' AND Password ='" + pass + "'";
+                clsDataAccess mylogin = new clsDataAccess();//Login class is called
+                SqlDataReader mydr1 = mylogin.Login(s);
+
+                //Response.Write(s + "<hr>3" + noErrorFlag + "<br>"); 
+
+                while (mydr1.Read())
+                {
+                    //Response.Write(s + "<hr>4" + noErrorFlag + "<br>"); 
+                    if (mydr1.GetValue(5).ToString().Trim() == "Admin")
+                    {
+                        Session["userfullname"] = mydr1.GetValue(3).ToString().Trim() + " " + mydr1.GetValue(4).ToString().Trim();
+                        Session["Admin"] = "true";
+                        Response.Redirect("DBMain.aspx");
+                    }
+                    else
+                    {
+                        lblError.Text = "Error ! - Invalid Login";
+                    }
+
+                }
+                mydr1.Close();
+                mylogin.closeConnection();
+
+
+            }
+
+            else
+            {
+                lblError.Text = "Input contains some special character !<br> Only a-z A-Z . _ s is allowed upto 50 characters";
+            }
+
+            lblError.Text = lblError.Text + "<hr>Error ! - Invalid Login ";
+
+
+
+        }
+        private bool CheckSQLInjection(string s)
+        {
+
+            bool flagSQLI = false;
+            try
+            {
+
+                if (!Regex.IsMatch(s, @"^[a-zA-Z._s]{1,50}$"))
+                {
+                    flagSQLI = false;
+                }
+                else
+                {
+                    flagSQLI = true;
+                }
+            }
+            catch (Exception)
+            {
+                flagSQLI = true;
+
+            }
+
+            if (flagSQLI)
+                return true;
+            else
+                return false;
+
+        }
+
+        private void btnLogin_Click(object sender, System.EventArgs e)
+        {
+            txtemail.Text = "";
+            txtpwd.Text = "";
+        }
+
+    }
+}
